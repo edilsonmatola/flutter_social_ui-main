a559855777d477f7b9826ee03cee8d78e4ad7c9a
Edilson Matola <edilsonmatola13@gmail.com>
2022-03-23 22:06:01 +0200

    Code Format

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primaryColor: Colors.blue,
       ),
-      home: LoginScreen(),
+      home: const LoginScreen(),
     );
   }
 }
