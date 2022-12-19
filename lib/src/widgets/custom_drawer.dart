b5484570dd36b8497fa12ee514e84f5048f999f4
Edilson Matola <edilsonmatola13@gmail.com>
2022-12-19 13:07:20 +0200

    Added Safe Area

--- lib/src/widgets/custom_drawer.dart
+++ lib/src/widgets/custom_drawer.dart
@@ -24,127 +24,129 @@ class CustomDrawer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Drawer(
-      child: Column(
-        children: [
-          Stack(
-            children: [
-              Image(
-                image: AssetImage(
-                  currentUser.backgroundImageUrl!,
+    return SafeArea(
+      child: Drawer(
+        child: Column(
+          children: [
+            Stack(
+              children: [
+                Image(
+                  image: AssetImage(
+                    currentUser.backgroundImageUrl!,
+                  ),
+                  fit: BoxFit.cover,
+                  height: 200,
+                  width: double.infinity,
                 ),
-                fit: BoxFit.cover,
-                height: 200,
-                width: double.infinity,
-              ),
-              Positioned(
-                bottom: 20,
-                left: 20,
-                child: Row(
-                  crossAxisAlignment: CrossAxisAlignment.end,
-                  children: [
-                    Container(
-                      height: 100,
-                      width: 100,
-                      decoration: BoxDecoration(
-                        shape: BoxShape.circle,
-                        border: Border.all(
-                          width: 3,
-                          color: Theme.of(context).primaryColor,
-                        ),
-                      ),
-                      child: ClipOval(
-                        child: Image(
-                          image: AssetImage(
-                            currentUser.profileImageUrl!,
+                Positioned(
+                  bottom: 20,
+                  left: 20,
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      Container(
+                        height: 100,
+                        width: 100,
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          border: Border.all(
+                            width: 3,
+                            color: Theme.of(context).primaryColor,
                           ),
-                          fit: BoxFit.cover,
+                        ),
+                        child: ClipOval(
+                          child: Image(
+                            image: AssetImage(
+                              currentUser.profileImageUrl!,
+                            ),
+                            fit: BoxFit.cover,
+                          ),
                         ),
                       ),
-                    ),
-                    SizedBox(
-                      width: 6,
-                    ),
-                    Text(
-                      currentUser.name!,
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 25,
-                        fontWeight: FontWeight.bold,
-                        letterSpacing: 1.5,
+                      SizedBox(
+                        width: 6,
                       ),
-                    ),
-                  ],
+                      Text(
+                        currentUser.name!,
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 25,
+                          fontWeight: FontWeight.bold,
+                          letterSpacing: 1.5,
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
+              ],
+            ),
+            /* HOME */
+            _buildDrawerOptions(
+              icon: Icon(
+                Icons.home_outlined,
               ),
-            ],
-          ),
-          /* HOME */
-          _buildDrawerOptions(
-            icon: Icon(
-              Icons.home_outlined,
-            ),
-            title: 'Home',
-            onTap: () => Navigator.pushReplacement(
-              context,
-              MaterialPageRoute(
-                builder: (_) => HomeScreen(),
-              ),
-            ),
-          ),
-          /* CHAT */
-          _buildDrawerOptions(
-            icon: Icon(Icons.chat_outlined),
-            title: 'Chat',
-            onTap: () {},
-          ),
-          /* LOCATION */
-          _buildDrawerOptions(
-            icon: Icon(
-              Icons.location_on_outlined,
-            ),
-            title: 'Location',
-            onTap: () {},
-          ),
-          /* PROFILE */
-          _buildDrawerOptions(
-            icon: Icon(
-              Icons.account_circle_outlined,
-            ),
-            title: 'Profile',
-            onTap: () => Navigator.pushReplacement(
-              context,
-              MaterialPageRoute(
-                builder: (_) => ProfileScreen(
-                  user: currentUser,
+              title: 'Home',
+              onTap: () => Navigator.pushReplacement(
+                context,
+                MaterialPageRoute(
+                  builder: (_) => HomeScreen(),
                 ),
               ),
             ),
-          ),
-          /* SETTINGS */
-          _buildDrawerOptions(
-            icon: Icon(Icons.settings_outlined),
-            title: 'Settings',
-            onTap: () {},
-          ),
-          Expanded(
-            child: Align(
-              /* Alinhamento no fundo central*/
-              alignment: FractionalOffset.bottomCenter,
-              child: /* LOGOUT */
-                  _buildDrawerOptions(
-                icon: Icon(Icons.logout_outlined),
-                title: 'Log Out',
-                onTap: () => Navigator.pushReplacement(
-                  context,
-                  MaterialPageRoute(
-                    builder: (_) => LoginScreen(),
+            /* CHAT */
+            _buildDrawerOptions(
+              icon: Icon(Icons.chat_outlined),
+              title: 'Chat',
+              onTap: () {},
+            ),
+            /* LOCATION */
+            _buildDrawerOptions(
+              icon: Icon(
+                Icons.location_on_outlined,
+              ),
+              title: 'Location',
+              onTap: () {},
+            ),
+            /* PROFILE */
+            _buildDrawerOptions(
+              icon: Icon(
+                Icons.account_circle_outlined,
+              ),
+              title: 'Profile',
+              onTap: () => Navigator.pushReplacement(
+                context,
+                MaterialPageRoute(
+                  builder: (_) => ProfileScreen(
+                    user: currentUser,
                   ),
                 ),
               ),
             ),
-          ),
-        ],
+            /* SETTINGS */
+            _buildDrawerOptions(
+              icon: Icon(Icons.settings_outlined),
+              title: 'Settings',
+              onTap: () {},
+            ),
+            Expanded(
+              child: Align(
+                /* Alinhamento no fundo central*/
+                alignment: FractionalOffset.bottomCenter,
+                child: /* LOGOUT */
+                    _buildDrawerOptions(
+                  icon: Icon(Icons.logout_outlined),
+                  title: 'Log Out',
+                  onTap: () => Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(
+                      builder: (_) => LoginScreen(),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
