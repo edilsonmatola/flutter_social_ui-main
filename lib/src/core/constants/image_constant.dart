a3ddd3565a85bd9482e2afe07233a7ea69d1bbb7
Edilson Matola <edilsonmatola13@gmail.com>
2022-12-19 13:02:34 +0200

    Constant Image Path

---
+++ lib/src/core/constants/image_constant.dart
@@ -0,0 +1,23 @@
+class ImageConstant {
+  // * Posts Images
+  static const String post0 = 'assets/images/post0.jpg';
+  static const String post1 = 'assets/images/post1.jpg';
+  static const String post2 = 'assets/images/post2.jpg';
+  static const String post3 = 'assets/images/post3.jpg';
+  static const String post4 = 'assets/images/post4.jpg';
+  static const String post5 = 'assets/images/post5.jpg';
+
+  // * Users Images
+  static const String user0 = 'assets/images/user0.jpg';
+  static const String user1 = 'assets/images/user1.jpg';
+  static const String user2 = 'assets/images/user2.jpg';
+  static const String user3 = 'assets/images/user3.jpg';
+  static const String user4 = 'assets/images/user4.jpg';
+  static const String user5 = 'assets/images/user5.jpg';
+  static const String user6 = 'assets/images/user6.jpg';
+
+  // * Current User
+  static const String currentUser = 'assets/images/user.jpg';
+  static const String currentUserBackgroundImage =
+      'assets/images/user_background.jpg';
+}
