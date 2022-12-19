2188d65023a36dcf66c373dd9ad11d5344afd1e4
Edilson Matola <edilsonmatola13@gmail.com>
2022-12-19 13:02:21 +0200

    Applying Image Constant File

--- lib/src/data/data.dart
+++ lib/src/data/data.dart
@@ -1,9 +1,10 @@
+import '../core/constants/image_constant.dart';
 import '../models/post_model.dart';
 import '../models/user_model.dart';
 
 // Posts
 final _post0 = Post(
-  imageUrl: 'assets/images/post0.jpg',
+  imageUrl: ImageConstant.post0,
   author: User(),
   title: 'Post 0',
   location: 'Location 0',
@@ -11,7 +12,7 @@ final _post0 = Post(
   comments: 37,
 );
 final _post1 = Post(
-  imageUrl: 'assets/images/post1.jpg',
+  imageUrl: ImageConstant.post1,
   author: User(),
   title: 'Post 1',
   location: 'Location 1',
@@ -19,7 +20,7 @@ final _post1 = Post(
   comments: 129,
 );
 final _post2 = Post(
-  imageUrl: 'assets/images/post2.jpg',
+  imageUrl: ImageConstant.post2,
   author: User(),
   title: 'Post 2',
   location: 'Location 2',
@@ -27,7 +28,7 @@ final _post2 = Post(
   comments: 213,
 );
 final _post3 = Post(
-  imageUrl: 'assets/images/post3.jpg',
+  imageUrl: ImageConstant.post3,
   author: User(),
   title: 'Post 3',
   location: 'Location 3',
@@ -35,7 +36,7 @@ final _post3 = Post(
   comments: 25,
 );
 final _post4 = Post(
-  imageUrl: 'assets/images/post4.jpg',
+  imageUrl: ImageConstant.post4,
   author: User(),
   title: 'Post 4',
   location: 'Location 4',
@@ -43,7 +44,7 @@ final _post4 = Post(
   comments: 74,
 );
 final _post5 = Post(
-  imageUrl: 'assets/images/post5.jpg',
+  imageUrl: ImageConstant.post5,
   author: User(),
   title: 'Post 5',
   location: 'Location 5',
@@ -53,21 +54,21 @@ final _post5 = Post(
 
 final posts = [_post0, _post1, _post2, _post3, _post4, _post5];
 final users = [
-  User(profileImageUrl: 'assets/images/user0.jpg'),
-  User(profileImageUrl: 'assets/images/user1.jpg'),
-  User(profileImageUrl: 'assets/images/user2.jpg'),
-  User(profileImageUrl: 'assets/images/user3.jpg'),
-  User(profileImageUrl: 'assets/images/user4.jpg'),
-  User(profileImageUrl: 'assets/images/user5.jpg'),
-  User(profileImageUrl: 'assets/images/user6.jpg'),
+  User(profileImageUrl: ImageConstant.user0),
+  User(profileImageUrl: ImageConstant.user1),
+  User(profileImageUrl: ImageConstant.user2),
+  User(profileImageUrl: ImageConstant.user3),
+  User(profileImageUrl: ImageConstant.user4),
+  User(profileImageUrl: ImageConstant.user5),
+  User(profileImageUrl: ImageConstant.user6),
 ];
 final _yourPosts = [_post1, _post3, _post5];
 final _yourFavorites = [_post0, _post2, _post4];
 
 // Current User
 final User currentUser = User(
-  profileImageUrl: 'assets/images/user.jpg',
-  backgroundImageUrl: 'assets/images/user_background.jpg',
+  profileImageUrl: ImageConstant.currentUser,
+  backgroundImageUrl: ImageConstant.currentUserBackgroundImage,
   name: 'Rebecca',
   following: 453,
   followers: 937,
